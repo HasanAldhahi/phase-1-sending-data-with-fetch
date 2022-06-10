45637188afe52888a7fd29509b63652134eb6967
Hasan <Hasanaldhahi3@gmail.com>
2022-06-11 00:09:23 +0300

    assignment is done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+const message = document.querySelector("#message")
+const error = document.querySelector("#error")
+
+function submitData(name, email) {
+
+    const data = {
+        name: name,
+        email: email,
+    }
+
+
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(data),
+    }).then(res => res.json()).then(user => {
+        message.innerHTML = user.id;
+    })
+        .catch(err => {
+            error.innerHTML = err.message
+            return err.message
+        })
+
+
+
+
+
+}
